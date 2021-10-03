a59bd6826f0b7f4fce9495f0da8b4808c62eff2f
bthehacker <78182394+bthehacker@users.noreply.github.com>
2021-10-03 15:56:10 +0400

    Create kbtr.sh

---
+++ scripts/kbtr.sh
@@ -0,0 +1,2 @@
+wget https://github.com/bthehacker/QuickScripts/blob/main/programs/arabicKbTranslator?raw=true
+wget https://github.com/bthehacker/QuickScripts/blob/main/programs/frenchKbTranslator?raw=true
