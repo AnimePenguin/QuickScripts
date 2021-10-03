d36bde76553f599aa6c4ccfd862b35769a19f75d
bthehacker <78182394+bthehacker@users.noreply.github.com>
2021-10-03 16:12:31 +0400

    Update kbtr.sh

--- scripts/kbtr.sh
+++ scripts/kbtr.sh
@@ -1,3 +1,17 @@
-wget -O /bin/arkbtr https://raw.githubusercontent.com/bthehacker/QuickScripts/main/programs/arabicKbTranslator
-wget -O /bin/frkbtr https://raw.githubusercontent.com/bthehacker/QuickScripts/main/programs/frenchKbTranslator
+echo "frkbtr and arkbtr Install Script
+by Sourav
+Version 1.0
+"
+
+echo "Installing arkbtr"
+wget -O /bin/arkbtr https://raw.githubusercontent.com/bthehacker/QuickScripts/main/programs/arabicKbTranslator &> /dev/null
+
+echo "Installing frkbtr"
+wget -O /bin/frkbtr https://raw.githubusercontent.com/bthehacker/QuickScripts/main/programs/frenchKbTranslator &> /dev/null
+
+echo "Finishing Up"
 chmod +x /bin/arkbtr /bin/frkbtr
+
+echo "Done"
+
+echo "Type arkbtr or frkbtr in the Terminal to Use this Program"
