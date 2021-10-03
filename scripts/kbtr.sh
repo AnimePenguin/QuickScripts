cd1e1d0704b15f6cfab6f95d5c2e3fa6d267ad74
bthehacker <78182394+bthehacker@users.noreply.github.com>
2021-10-03 16:06:45 +0400

    Update kbtr.sh

--- scripts/kbtr.sh
+++ scripts/kbtr.sh
@@ -1,3 +1,3 @@
 wget -O /bin/arkbtr https://raw.githubusercontent.com/bthehacker/QuickScripts/main/programs/arabicKbTranslator
 wget -O /bin/frkbtr https://raw.githubusercontent.com/bthehacker/QuickScripts/main/programs/frenchKbTranslator
-chmod +x /bin/arkbtr /bin/arkbtr 
+chmod +x /bin/arkbtr /bin/frkbtr
