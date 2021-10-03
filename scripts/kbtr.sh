38338f56e5e23029074bead9c4e8f18e56def8a5
bthehacker <78182394+bthehacker@users.noreply.github.com>
2021-10-03 16:16:27 +0400

    Update kbtr.sh

--- scripts/kbtr.sh
+++ scripts/kbtr.sh
@@ -3,10 +3,10 @@ by Sourav
 Version 1.0
 "
 
-echo "Installing arkbtr"
+echo "Installing arkbtr. It will take 5 - 10 secs"
 wget -O /bin/arkbtr https://raw.githubusercontent.com/bthehacker/QuickScripts/main/programs/arabicKbTranslator &> /dev/null
 
-echo "Installing frkbtr"
+echo "Installing frkbtr. It will take 5 - 10 secs"
 wget -O /bin/frkbtr https://raw.githubusercontent.com/bthehacker/QuickScripts/main/programs/frenchKbTranslator &> /dev/null
 
 echo "Finishing Up"
@@ -14,4 +14,5 @@ chmod +x /bin/arkbtr /bin/frkbtr
 
 echo "Done"
 
-echo "Type arkbtr or frkbtr in the Terminal to Use this Program"
+echo "
+Type arkbtr or frkbtr in the Terminal to Use this Program"
